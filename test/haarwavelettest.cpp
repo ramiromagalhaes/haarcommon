4ddc87058a2f3ffc5ccce455556b88e913d90541
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2014-06-22 21:32:01 -0300

    Testing the internsity normalized haar wavelets.

--- test/haarwavelettest.cpp
+++ test/haarwavelettest.cpp
@@ -12,32 +12,33 @@
 
 const cv::Mat getMockImage()
 {
+    //Values taken from file "integrals.ods" (in the test folder)
     cv::Mat image(5, 5, cv::DataType<unsigned char>::type);
     image.at<unsigned char>(0, 0) = 5;
-    image.at<unsigned char>(0, 1) = 5;
-    image.at<unsigned char>(0, 2) = 5;
-    image.at<unsigned char>(0, 3) = 5;
-    image.at<unsigned char>(0, 4) = 5;
-    image.at<unsigned char>(1, 0) = 4;
-    image.at<unsigned char>(1, 1) = 4;
-    image.at<unsigned char>(1, 2) = 4;
-    image.at<unsigned char>(1, 3) = 4;
-    image.at<unsigned char>(1, 4) = 4;
-    image.at<unsigned char>(2, 0) = 3;
-    image.at<unsigned char>(2, 1) = 3;
-    image.at<unsigned char>(2, 2) = 3;
-    image.at<unsigned char>(2, 3) = 3;
-    image.at<unsigned char>(2, 4) = 3;
-    image.at<unsigned char>(3, 0) = 4;
+    image.at<unsigned char>(0, 1) = 100;
+    image.at<unsigned char>(0, 2) = 12;
+    image.at<unsigned char>(0, 3) = 30;
+    image.at<unsigned char>(0, 4) = 200;
+    image.at<unsigned char>(1, 0) = 0;
+    image.at<unsigned char>(1, 1) = 13;
+    image.at<unsigned char>(1, 2) = 12;
+    image.at<unsigned char>(1, 3) = 150;
+    image.at<unsigned char>(1, 4) = 20;
+    image.at<unsigned char>(2, 0) = 180;
+    image.at<unsigned char>(2, 1) = 5;
+    image.at<unsigned char>(2, 2) = 90;
+    image.at<unsigned char>(2, 3) = 150;
+    image.at<unsigned char>(2, 4) = 12;
+    image.at<unsigned char>(3, 0) = 190;
     image.at<unsigned char>(3, 1) = 4;
-    image.at<unsigned char>(3, 2) = 4;
-    image.at<unsigned char>(3, 3) = 4;
-    image.at<unsigned char>(3, 4) = 4;
-    image.at<unsigned char>(4, 0) = 5;
+    image.at<unsigned char>(3, 2) = 160;
+    image.at<unsigned char>(3, 3) = 30;
+    image.at<unsigned char>(3, 4) = 5;
+    image.at<unsigned char>(4, 0) = 170;
     image.at<unsigned char>(4, 1) = 5;
-    image.at<unsigned char>(4, 2) = 5;
+    image.at<unsigned char>(4, 2) = 100;
     image.at<unsigned char>(4, 3) = 5;
-    image.at<unsigned char>(4, 4) = 5;
+    image.at<unsigned char>(4, 4) = 90;
 
     return image;
 }
@@ -84,8 +85,8 @@ const MyHaarWavelet getMyWavelet()
     weight[1] = -1;
 
     std::vector<float> means(2);
-    means[0] = .013725490196f;
-    means[1] = .017647058824f;
+    means[0] = .117647058824f;
+    means[1] = .127450980392f;
 
     return MyHaarWavelet(rects, weight, means);
 }
@@ -102,12 +103,12 @@ BOOST_AUTO_TEST_CASE(IntensityNormalizedWaveletEvaluatorTest)
     IntensityNormalizedWaveletEvaluator evaluator;
     {
         const HaarWavelet wavelet = getHaarWavelet();
-        BOOST_CHECK_EQUAL(evaluator(wavelet, integralSum, integralSquare), -0.00392156933f);
+        BOOST_CHECK_CLOSE(evaluator(wavelet, integralSum, integralSquare), -.009803921569f, 0.0001f);
     }
 
     {
         const MyHaarWavelet wavelet = getMyWavelet();
-        BOOST_CHECK_EQUAL(evaluator(wavelet, integralSum, integralSquare), 0);
+        BOOST_CHECK_CLOSE(evaluator(wavelet, integralSum, integralSquare), 0, 0.00001f);
     }
 }
 
